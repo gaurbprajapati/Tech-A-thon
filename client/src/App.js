24a67d3bc280ed5bf8265dd292c0dd9758bafeae
Gaurav Prajapati <gauravprajapati094@gmail.com>
2022-09-25 02:35:38 +0530

    adding bacdend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import DefaultLayout from './components/DefaultLayout';
 import './App.css';
 import 'antd/dist/antd.less';
 import { Button } from 'antd';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
+
   return (
     <>
-      <h1>Helo</h1>
-      <Button type="primary">Primary Button</Button>
+      <DefaultLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DefaultLayout from '../components/DefaultLayout';
+
+function Home() {
+    return (
+        <>
+            {/* <DefaultLayout /> */}
+
+            {/* </DefaultLayout> */}
+        </>
+    )
+}
+
+export default Home
